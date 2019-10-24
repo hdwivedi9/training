ee2b3b4338cd911dbd55df9553c5374a6b112127
Himanshu <himandwivedi@gmail.com>
2019-10-24 19:30:15 +0530

    elasticsearch final with one bug

--- back-end/app/Providers/AppServiceProvider.php
+++ back-end/app/Providers/AppServiceProvider.php
@@ -14,8 +14,28 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->bind(
-            \App\Repositories\Interfaces\ArticleInterface::class,
-            \App\Repositories\ArticleRepository::class
+            \App\Repositories\Interfaces\ArticleInterface::class, function () {
+                // This is useful in case we want to turn-off our
+                // search cluster or when deploying the search
+                // to a live, running application at first.
+                if (! config('services.search.enabled')) {
+                    return new \App\Repositories\ArticleRepository();
+                }
+
+                return new \App\Repositories\ElasticsearchRepository(
+                    $app->make(Client::class)
+                );
+            }
         );
+        $this->bindSearchClient();
+    }
+    
+    private function bindSearchClient()
+    {
+        $this->app->bind(Client::class, function ($app) {
+            return ClientBuilder::create()
+                ->setHosts($app['config']->get('services.search.hosts'))
+                ->build();
+        });
     }
 }
